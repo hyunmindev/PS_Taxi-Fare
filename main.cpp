4a1a042bb0afc4a47f8ae9df58026f9d6328c378
Hyunmin Jung <jung@hyunmin.dev>
2021-03-27 18:35:41 +0900

    Setup project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(int node_count, int s, int a, int b, vector<vector<int>> fare_vec) {
+  int output = 0;
+  return output;
+}
+
+int main() {
+  cout << solution(6, 4, 6, 2,
+                   {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 42}, {5, 1, 24}, {4, 6, 50},
+                       {2, 4, 66}, {2, 3, 22}, {1, 6, 25}}) << endl;
+//  cout << solution(7,3,4,1,
+//                   {{5, 7, 9}, {4, 6, 4}, {3, 6, 1}, {3, 2, 3}, {2, 1, 6}}) << endl;
+  return 0;
+}
